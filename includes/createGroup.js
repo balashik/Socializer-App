aba9e3294bc872cda884b2e9a841a348932e905f
balashik <galshalit@gmail.com>
2015-06-19 12:56:24 +0300

    first commit

---
+++ includes/createGroup.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+
+    $("#topLinkR").click(function(){
+        var groupName = $("input[name='groupname']").val();
+        var frequency = $("input[name='frequency']").val();
+        var cf = frequency.charAt(0);
+        var mf = frequency.charAt(1);
+        var wf = frequency.charAt(2);
+
+
+        var group={
+            name:groupName,
+            contacts:[],
+            frequency:{calls:cf,sms:mf,whatsapp:wf,type:1},
+            lastReset:Date.now()
+        }
+
+
+        
+        var allData = {
+            key: 123123,
+            pn: 0543191901,
+            group: group
+        }
+        
+        var json = JSON.stringify(allData);
+        
+        var url = "http://socializerapp.herokuapp.com/createGroup";
+        
+       /* $.post(url, groupJson, function(data) {
+            console.log(data);
+        }, "jsonp");
+        */
+        $.ajax({
+            type: "POST",
+            url: "http://socializerapp.herokuapp.com/createGroup",
+            data: json,
+            contentType: 'application/json',
+            crossDomain: true,
+            dataType: "json",
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            },
+            success: function(data) {
+                console.log(data);
+            },
+        });
+        
+        
+
+
+    });
+});
+
