cdcf596be010c9c160c6c66bee6fc763b697efe9
balashik <galshalit@gmail.com>
2015-07-07 13:23:50 +0300

    app script+ index + css update

--- socializer/www/js/appScript.js
+++ socializer/www/js/appScript.js
@@ -15,7 +15,7 @@ app.controller('bodyController',function($scope,$http){
             
            
             
-            $.getJSON("./includes/js/contacts.json", function(data){
+            $.getJSON("./js/contacts.json", function(data){
                 var contactsLetters= setLetters(data);
                 
                 for(var i=0;i<contactsLetters.length;i++){
@@ -51,3 +51,40 @@ function setLetters(data){
 }
 
 
+/*
+
+$( document ).ready(function() {
+	var selectedContacts=[];
+    var abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+	if(selectedContacts.length==0) {
+		$("#topLinkR").append("Choose");
+	}
+	else {
+		$("#topLinkR").append("Next");
+	}
+	
+
+
+    $.getJSON("./includes/contacts.json", function(JSON){
+        
+        abc.forEach(function(l){
+            var lDiv = $("<div id='letterTitles' ><\div>");
+            lDiv.html(l);
+            $("#contacts").append(lDiv);
+            for(i=0;i< JSON.contactList.length ;i++){
+                if(l==JSON.contactList[i].name.charAt(0).toUpperCase()){
+                    var div = $("<div id='contactDiv' ><\div>");
+                    div.html(JSON.contactList[i].name);
+                    $("#contacts").append(div);
+                }
+			
+		      }
+        });
+        
+
+
+    });
+});
+
+
+*/
