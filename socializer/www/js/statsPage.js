f0f9465882b146a73400319f3085ed28747e7c78
balashik <galshalit@gmail.com>
2015-07-24 11:14:17 +0300

    jquery ui fix

--- socializer/www/js/statsPage.js
+++ socializer/www/js/statsPage.js
@@ -9,8 +9,13 @@ function initStatsPage(){
     $("#listView").css("background-color","#252a3d");
     $("#statsImg").css("background-image","url(./css/images/stats.png)");
     $("#listImg").css("background-image","url(./css/images/list.png)");
-    $(".wrapper").append("<nav id='newFrequencyNav'></nav>");
+    $(".wrapper").append("<div id='topStats'></div>");
+    $("#topStats").append("<nav id='newFrequencyNav'></nav>");
     $("#newFrequencyNav").append("<ul><li><a>Weekly</a></li><li><a>Biweekly</a></li><li><a>Monthly</a></li></ul>");
+    $("#topStats").append("<div id='topStatsPrograssBar'></div>");
+    $("#topStatsPrograssBar").progressbar({
+        value:65
+    });
     
     $("#listView").click(function(){
         $("div").remove("#listView");
