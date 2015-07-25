74b2554f01202694fb78d750b0822d6d32c412ca
OppositeVector <iloriginal@gmil.com>
2015-07-25 15:33:19 +0300

    storage class ready for use

--- socializer/www/js/cordovaAdditions.js
+++ socializer/www/js/cordovaAdditions.js
@@ -21,33 +21,31 @@ if(window._cordovaNative) {
 	var logger= $("<div>");
 	$("body").prepend(logger);
 
-	var storage = {
-		key: null,
-		write: function(field, data) {
+	function Storage() {
+		this.write = function(field, data) {
 			db.transaction(function(tx) {
 				tx.executeSql("INSERT INTO main_table (id, data) VALUES (?,?)", {field, data}, function(tx, res) {
 					console.log("insertId: " + res.insertId);
       				console.log("rowsAffected: " + res.rowsAffected);
 				});
 			});
-		},
-		read: function(field, callback) {
+		}
+		this.read = function(field, callback) {
 			db.transaction(function(tx) {
 				tx.executeSql("SELECT data FROM main_table WHERE id = " + field, function(tx, res) {
 					callback(res);
 				});
 			});
 		}
-	}
-	var db = window.sqlitePlugin.openDatabase({name: "socializer.db", androidDatabaseImplementation: 2, androidLockWorkaround: 1});
-	db.transaction(function(tx) {
-		tx.executeSql('CREATE TABLE IF NOT EXISTS main_table (id text primary key, data text)');
-		db.executeSql("pragma table_info (test_table);", [], function(res) {
-	      console.log("PRAGMA res: " + JSON.stringify(res));
-	    });
-		db.executeSql("SELECT data FROM main_table WHERE id = 'key'", function(tx, res) {
 
+		var db = window.sqlitePlugin.openDatabase({name: "socializer.db", androidDatabaseImplementation: 2, androidLockWorkaround: 1});
+		db.transaction(function(tx) {
+			tx.executeSql('CREATE TABLE IF NOT EXISTS main_table (id text primary key, data text)');
+			db.executeSql("pragma table_info (test_table);", [], function(res) {
+		      console.log("PRAGMA res: " + JSON.stringify(res));
+		    });
 		});
-	});
+	}
 
+	var storage = new Storage();
 }
